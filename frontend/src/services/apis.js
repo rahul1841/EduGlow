48a241044afef285b60c9da61d3b79b0a73eb94a
rahul1841 <rahul2004kumar14@gmail.com>
2024-04-27 16:57:30 +0530

    url update

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://eduglow.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
